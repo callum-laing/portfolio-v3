4a0db669b8c38396092c3b95e7f710bcf2773491
Callum Laing <callum.laing91@hotmail.co.uk>
2024-03-21 18:30:57 +0000

    added components

---
+++ src/components/Hero.svelte
@@ -0,0 +1,24 @@
+<main>
+    <div class="intro">
+        <h1>Hey, I'm <span>Callum</span>, a Web Developer crafting tomorrow's web landscapes, one pixel at a time.</h1>
+    </div>
+</main>
+
+<style>
+.intro {
+font-size: 2rem;
+width: 50%;
+}
+
+span {
+    color: rgba(200, 250, 20);
+    font-size: 1.2em;
+}
+
+@media (max-width: 769px) {
+    .intro {
+        font-size: 1rem;
+        width: 100%;
+    }
+}
+</style>
